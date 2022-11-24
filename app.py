6099b407ea5af79af7b0c063c5248d1e61aa22b7
IvanDorokhov <62538181+IvanDorokhov@users.noreply.github.com>
2022-11-24 15:04:36 +0700

    1

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('my_best_pipeline_NB')
+cols = ['gender','age','hypertension','heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status']
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 1)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred='Вероятность инсульта {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
